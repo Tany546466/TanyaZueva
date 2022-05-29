4fa0949a6a79d7460f735d883296e129a157d031
Sendy31 <105802056+Sendy31@users.noreply.github.com>
2022-05-29 18:24:55 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-misha
+def brackets(q):
+    i = 0
+    a = 0
+
+    while i != len(q) and a >= 0:
+        if q[i] == '(':
+            a += 1
+        elif q[i] == ')':
+            a -= 1
+        i += 1
+
+    if a == 0:
+        return True
+    else:
+        return False
+
+
+s = input()
+print(brackets(s))
